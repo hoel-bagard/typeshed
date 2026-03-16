a31fef087defdf5b04efb98ae208d54570daa2b4
Hoel Bagard <hoel.bagard@gmail.com>
2026-03-16 23:02:54 +0900

    fix: add missing __getattr__

--- stubs/behave/behave/fixture.pyi
+++ stubs/behave/behave/fixture.pyi
@@ -1,3 +1,4 @@
+from _typeshed import Incomplete
 from collections.abc import Callable
 from typing import Any, Concatenate, ParamSpec, TypeVar
 
@@ -11,3 +12,4 @@ def use_fixture(
     fixture_func: Callable[Concatenate[Context, _P], _T], context: Context, *fixture_args: _P.args, **fixture_kwargs: _P.kwargs
 ) -> _T: ...
 def fixture(func: _F | None = None, name: str | None = None, pattern: str | None = None) -> _F: ...
+def __getattr__(name: str) -> Incomplete: ...
